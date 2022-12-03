df34fe477c01e7eebbc70fcd0bc6c9b8908d13cf
Tusar <tusarahmed141@gmail.com>
2022-12-03 14:14:06 +0600

    Created a Element styled components

--- src/Components/CardSection.js
+++ src/Components/CardSection.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { CardContainer, StyledCardSection } from "../Styled_Components/Container.styles";
+import {
+  CardContainer,
+  StyledCardSection,
+} from "../Styled_Components/Container.styles";
+import { desktopImg } from "../assets/images/image-product-desktop.jpg";
 
 const CardSection = () => {
   return (
     <StyledCardSection>
       <CardContainer>
-        <h2>Hello World</h2>
+        <CardImage>
+          <Image src={desktopImg} />
+        </CardImage>
       </CardContainer>
     </StyledCardSection>
   );
